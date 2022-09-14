212f65fbf6c56355b3590fdded23881f7ad814c1
AONSMS <113531042+AONSMS@users.noreply.github.com>
2022-09-14 16:27:35 +0200

    Update FittingT2*WithRicianNoise.py

--- FittingT2*WithRicianNoise.py
+++ FittingT2*WithRicianNoise.py
@@ -10,17 +10,6 @@ import glob
 import json
 import matplotlib.pyplot as plt 
 
-#takes the time indicated for every echo
-def getEchoTimes(n_echos,ikputPath):
-    TE = list()	#create a list where we'll put each and every echo time per visit
-    for echo in range(n_echos):
-        json_file=glob.glob(ikputPath + '*echo-' +"%02d" % (echo+1) +'_part-mag_MEGRE.json') #echo time is put in the .json file with the fitting name
-        fp = open( json_file[0] ) 
-        hdr=json.load(fp)
-        fp.close()
-        TE.append(hdr["EchoTime"]) #add the echo time we just took in a list
-    return TE #save our result
-
 def fit_Gauss_Newton_3D(y, a0=1.0, b0=1.0, tol=1e-6, num_iter=10):
     # fittinf a 3D + t volume to a*b^k
     # then, take R2* = -log(b)/dTE
@@ -82,35 +71,31 @@ def fit_Gauss_Newton_3D(y, a0=1.0, b0=1.0, tol=1e-6, num_iter=10):
     return x   
 
 FinalMT2=[] #(n_sub=200,n_iter,n_echos)
-v = 4
-s = 10
-N = 1# the standard deviation of the normal distribution
-sigma=N
+sigma = 1
 mu=0
 S0=30      #Base signal
 #S0=500      #Base signal
 T2_star_list=list(range(1,201))
-TE = getEchoTimes(10,"/mnt/senior/rawdata/sub-007/ses-7TV08/anat/") #We get a list with all the TE we need
+TE = (0.005,0.01,0.015,0.020,0.025,0.030)
+
 for a in range(len(T2_star_list)): #Apply all the algorithm for different T2 value
 
     theoS=[]#theoS will be the list where we'll put all the datas we need for our figure
-    for b in range(len(TE)):       
+    for b in range(len(TE)): #loop to obtain the associated signal depending of the T2 in this iteration      
         S=S0*np.exp(-TE[b]*1000/T2_star_list[a]) #ms #equation giving the signal value
         theoS.append(S) # add the result to theoS  
-   
-    List_Meas_T2star=[] # (n_iter,n_echos)
-    
+       
+    n_iter=100000 #number of iteration we want (objectif=100000)
     count=0
-    n_iter=100000
-    while count<n_iter: #number of iteration we want (objectif=100000)
+    List_Meas_T2star=[] # (n_iter,n_echos)
+    while count<n_iter: #loop to create our of 100000 noised datas
       theoSandNoise=[] #(10=n_echos)
       for c in range(len(theoS)):  
-          noise = np.random.normal(mu, sigma, 1) + 1j*np.random.normal(mu, sigma, 1)
+          noise = np.random.normal(mu, sigma, 1) + 1j*np.random.normal(mu, sigma, 1) #our way of generating rician noise
           theoSandNoise.extend(np.abs(theoS[c] + noise))    #add noise to our theorical values
           #theoSandNoise.extend(theoS[c] + np.linalg.norm(np.random.normal(scale=s, size=(N, 2)) + [[v,0]], axis=1))    #add noise to our theorical values
-      w=np.asarray(theoSandNoise)     
       List_Meas_T2star.append(theoSandNoise)
-      theoSandNoise=[]
+      theoSandNoise=[]#reset our list for the next iteration
       count+=1
         
     FinalMT2.append(List_Meas_T2star)
@@ -125,7 +110,7 @@ MT2star = 1000/MR2star
 
 MT2starMean=[]
 for i in range(len(MT2star)):
-   MT2starMean.append(np.average(MT2star[i]))
+   MT2starMean.append(np.average(MT2star[i]))#to obtain the mean of our measured T2*, thanks to which we will see the result of our algorithm compared to the theorical values
 MT2starMeanStD=np.std(MT2star,1)#Calculate standard deviation of MT2star (orange on the plot)
 MT2starMean=np.asarray(MT2starMean)
 #Create the abscisse list of same size than MT2star to create the scatter plot, contain T2* Theorical values :
@@ -139,13 +124,13 @@ for a in range(len(T2_star_list)):
 plt.figure()
 fig, ax = plt.subplots()
 plt.xlabel('T2* Théorique')
-plt.xlim([0, 200])
+plt.xlim([0, 200]) #set the limit in the physiological values
 plt.ylabel('T2*mesuré')
-plt.ylim([0, 200])
+plt.ylim([0, 200]) #set the limit in the physiological values
 plt.title('T2* mesuré et théorique'+' : '+str(n_iter)+' itérations')
-plt.scatter(abscisse,MT2star)
-plt.errorbar(T2_star_list,MT2starMean,MT2starMeanStD,c='orange')
-plt.plot(T2_star_list,T2_star_list,'-', c='red')
+plt.scatter(abscisse,MT2star) #points obtained during Monte Carlo
+plt.errorbar(T2_star_list,MT2starMean,MT2starMeanStD,c='orange') #measured T2* curve 
+plt.plot(T2_star_list,T2_star_list,'-', c='red') #theorical curve
 plt.show
 plt.savefig(str(n_iter)+'T2fittingRicianNoiseVersiondeLudovic')
 """
@@ -153,26 +138,13 @@ plt.savefig(str(n_iter)+'T2fittingRicianNoiseVersiondeLudovic')
 plt.figure()
 fig, ax = plt.subplots()
 plt.xlabel('T2* théorique (ms)')
-plt.xlim([0, 200])
+plt.xlim([0, 200]) #set the limit in the physiological values
 plt.ylabel('DeltaT2* mesuré / T2* mesuré (%)')
-plt.ylim([0, 40])
+plt.ylim([0, 40]) #limit arbitrarly set to give a readable image
 plt.title("Importance de l'incertitude en fonction du T2* théorique"+' : '+str(n_iter)+' itérations')
-plt.plot(T2_star_list,100*MT2starMeanStD/MT2starMean,'-', c='red')
+plt.plot(T2_star_list,100*MT2starMeanStD/MT2starMean,'-', c='red') 
 plt.show
-#plt.savefig(str(n_iter)+'ImportanceIncertitudeT2ricienVersiondeLudovic')
+#plt.savefig(str(n_iter)+'ImportanceIncertitudeT2)
 
 bias=np.polyfit(T2_star_list,MT2starMean,1)
 print ('BIAS : '+ str(bias))
-"""
-plt.figure()
-fig, ax = plt.subplots()
-plt.xlabel('T2* Théorique')
-plt.xlim([0, 200])
-plt.ylabel('T2*mesuré')
-plt.ylim([0, 200])
-plt.title('T2* mesuré et théorique'+' : '+str(n_iter)+' itérations')
-plt.scatter(abscisse,MT2star)
-plt.errorbar(T2_star_list,MT2starMean,MT2starMeanStD,c='orange')
-plt.plot(T2_star_list,T2_star_list,'-', c='red')
-plt.show
-"""
